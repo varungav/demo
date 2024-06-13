b0c092b2236d69074d025d072febd982c16d79bd
Varun <varun.g8866@gmail.com>
2024-06-13 22:14:50 +0530

    first comit

---
+++ src/test/java/net/javaguides/registration_login_spring_boot_security_thymeleaf/RegistrationLoginSpringBootSecurityThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.registration_login_spring_boot_security_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginSpringBootSecurityThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
